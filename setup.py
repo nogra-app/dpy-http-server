efc3dd2bf9f8fe7231b4d45c80fa341d22f9a74c
Argon <laiyeqi@gmail.com>
2026-05-24 17:13:50 +0800

    Refactor setup.py to read metadata from init.py

--- setup.py
+++ setup.py
@@ -1,36 +1,31 @@
 import os
+import re
 
 from setuptools import setup
 
-import server
 
-
-with open(os.path.abspath("./README.md"), "r") as file:
+with open(os.path.abspath("./README.md"), "r", encoding="utf-8") as file:
     readme = file.read()
 
+with open(os.path.abspath("./server/__init__.py"), "r", encoding="utf-8") as file:
+    init_py = file.read()
+
+version = re.search(r'__version__\s*=\s*[\'"]([^\'"]+)[\'"]', init_py).group(1)
+author = re.search(r'__author__\s*=\s*[\'"]([^\'"]+)[\'"]', init_py).group(1)
+license_ = re.search(r'__license__\s*=\s*[\'"]([^\'"]+)[\'"]', init_py).group(1)
 
 setup(
     name="dpy-http-server",
-    version=server.__version__,
+    version=version,
     description="Efficiently and intuitively create and manage an HTTP web server running in tandem with a discord.py library bot",
     long_description=readme,
     long_description_content_type="text/markdown",
     url="https://gitlab.com/marwynnsomridhivej/dpy-http-server",
-    author=server.__author__,
-    license=server.__license__,
-    classifiers=[
-        "License :: OSI Approved :: MIT License",
-        "Development Status :: 4 - Beta",
-        "Intended Audience :: Developers",
-        "Natural Language :: English",
-        "Programming Language :: Python :: 3.6",
-        "Programming Language :: Python :: 3.7",
-        "Programming Language :: Python :: 3.8",
-        "Programming Language :: Python :: 3.9",
-    ],
+    author=author,
+    license=license_,
     packages=["server"],
     include_package_data=True,
     install_requires=[
         "aiohttp",
-    ]
+    ],
 )
